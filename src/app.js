4f1447f983cfa777558f28ad9cdcf89f243e4a74
plutowang <pluto.wbc@gmail.com>
2020-05-05 22:30:08 -0400

    feat(boilerplate): boilerplate

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// reset css
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+const jsx = <h1>Blogs</h1>;
+
+ReactDOM.render(jsx, document.getElementById("app"));
